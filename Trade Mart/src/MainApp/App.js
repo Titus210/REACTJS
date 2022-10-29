56f6be23c8afb80d577a642e77a012f3f67861d2
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-10-29 10:44:13 +0300

    Main App

---
+++ Trade Mart/src/MainApp/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from '../Components/Header/Head'
+
+ import NavBar from '../Components/Header/NavBar'
+// import Login from '../Components/LandingPage/SignIn/SignIn'
+
+import "./app.css"
+
+
+function App() {
+  return (
+    <div>
+   <Head/>
+   <NavBar/>
+     
+    </div>
+  )
+}
+
+export default App
