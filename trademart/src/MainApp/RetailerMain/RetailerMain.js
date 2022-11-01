62dbf0ed604b4677033639f40260c7a3bafa310b
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-01 15:51:26 +0300

    Start Project

---
+++ trademart/src/MainApp/RetailerMain/RetailerMain.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class RetailerMain extends Component {
+  render() {
+    return (
+      <div>RetailerMain</div>
+    )
+  }
+}
+
+export default RetailerMain
---
+++ trademart/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+/*-------------Components Import--------------*/
+// Login COmponent
+import Login from '../src/MainApp/Login/Login'
+
+
+
+// Render To Index HTML
+ReactDOM.render(
+    <React.StrictMode>
+        <div>Hello</div>
+    </React.StrictMode>,
+    document.getElementById("trade-mart")
+)
