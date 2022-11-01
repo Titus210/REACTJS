5bf6d1c49a4a2a5e22ec677b2c58a6f82697c7f7
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-01 20:51:06 +0300

    App

--- trademart/src/MainApp/App/App.js
+++ trademart/src/MainApp/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
-
-
 import { BrowserRouter as Router } from "react-router-dom"
+
+// Importing Components
+import Header from '../Header/Header';
 
 
 export class App extends Component {
     render() {
         return (
             <Router>
+                <Header/>
             </Router>
         )
     }
