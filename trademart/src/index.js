f1606e2d0333e33a2e09be9e40e9f7c12d4fcb08
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-01 21:47:05 +0300

    App confiCss configg

--- trademart/src/index.js
+++ trademart/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./MainApp/App/App"
+
 
 /*-------------Components Import--------------*/
-// Login COmponent
 
+import App from "./MainApp/App/App"
 
 
 
