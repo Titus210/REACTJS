590dc5f746f61455f49c886b79c8da5627c5aa9d
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-01 21:25:47 +0300

    Retailer

--- trademart/src/MainApp/RetailerMain/RetailerMain.js
+++ trademart/src/MainApp/RetailerMain/RetailerMain.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
+// Component Imports
+
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import Pages from '../Pages/Pages'
+
 export class RetailerMain extends Component {
   render() {
     return (
-      <div>RetailerMain</div>
+      <>
+      <Header/>
+      <Pages/>
+      <Footer/>
+      </>
     )
   }
 }
