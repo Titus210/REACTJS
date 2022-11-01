d17891b8cbdd56b8ea6cb43e1dae26c61c56df28
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-01 21:52:44 +0300

    Configs

--- trademart/src/MainApp/Pages/Pages.js
+++ trademart/src/MainApp/Pages/Pages.js
@@ -1,12 +1,13 @@
 import React from 'react'
+
+// Compnent Imports
+import Category from './LandingCategory/Category'
 import AboutUs from './About/About'
 import FlashSale from './FlashSale/FlashSale'
 import Furniture from './Furniture/Furniture'
 import Grocery from './Grocery/Grocery'
 import Interior from './Interior/Interior'
 
-// Compnent Imports
-import Category from './LandingCategory/Category'
 
 function Pages() {
   return (
