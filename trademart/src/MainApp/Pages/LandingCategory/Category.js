f957498dfa2b692cb1b4d4ce14a10365a7b124fc
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-02 00:01:01 +0300

    Category

--- trademart/src/MainApp/Pages/LandingCategory/Category.js
+++ trademart/src/MainApp/Pages/LandingCategory/Category.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
-function Category() {
+import "./CategoryStyles/Category.css"
+
+
+
+function Categories() {
+  const data = [
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Electronics",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Groceries",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Electronics",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Furniture",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Interior",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Real Estate",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Motors & Cars",
+    },
+    {
+      cateImg: require("../Grocery/GroceryImages/apples.jpg"),
+      cateName: "Fashion",
+    }
+  ]
   return (
-    <div>Category</div>
+    <div className="navCategory">
+      {
+        data.map((value, index) => {
+          return (
+            <div className="box f_flex" key={index}>
+              <img src={value.cateImg} alt="No" />
+              <span>{value.cateName}</span>
+
+            </div>
+          )
+        })
+      }
+
+    </div>
   )
 }
 
-export default Category;
+export default Categories;
