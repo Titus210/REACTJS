67b531c23526908a0d323ebe8353008de1970e72
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-07 20:21:47 +0300

    furniture

---
+++ trademart/src/MainApp/Pages/Furniture/FurnitureWHolesaler/DisplayItems/couch.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import "../../../../WholesalersMain/Common/categories.css"
+function Couch() {
+    const FlashData = [
+        {
+            itemImage: require("../../FurnitureImages/couch_1.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_2.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_3.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_4.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_5.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_1.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+        {
+            itemImage: require("../../FurnitureImages/couch_7.jpg"),
+            ItemName: "Apples",
+            ItemPrice: 1200,
+            itemCategory: "Vegetables"
+        },
+
+    ]
+
+
+
+    return (
+
+        <section className="FlashSale">
+            <div className="flashHeader">
+                <h3>Chair </h3>
+
+            </div>
+            <div className="FlashWrapper">
+
+                {
+                    FlashData.map((value, index) => {
+                        return (
+
+                            <div className="FlashCard">
+                                <div className="product">
+                                    <img height="100px" src={value.itemImage} alt="" />
+                                    <div className="description">
+                                        <span>{value.itemName}</span>
+                                        <h5>{value.ItemPrice}</h5>
+                                        <div className="star">
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                            <i className="fa fa-star"></i>
+                                        </div>
+                                        <div className="add-cart">
+                                            <i class="fa-solid fa-heart"></i>
+                                            <button class="visit-button">View Product</button>
+                                            <i class="far fa-shopping-cart"></i>
+                                            </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
+
+
+        </section>
+    )
+
+}
+export default Couch
--- trademart/src/MainApp/Pages/Furniture/FurnitureWHolesaler/FurnitureWholesaler.js
+++ trademart/src/MainApp/Pages/Furniture/FurnitureWHolesaler/FurnitureWholesaler.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import ContactUs from '../../../WholesalersMain/Common/ContactUS/ContactUs'
+import Footer from '../../../WholesalersMain/Common/footer'
 import CommonHeader from '../../../WholesalersMain/Common/Header'
+
+import Bed from './DisplayItems/bed'
 import Chairs from './DisplayItems/chairs'
+import Couch from './DisplayItems/couch'
+import Inter from './DisplayItems/inter'
+import SliderHome from './DisplayItems/Slider/Slideritems'
+
 
 
 class FurnitureWholesaler extends Component {
@@ -10,22 +17,23 @@ class FurnitureWholesaler extends Component {
         <>
             <CommonHeader/>
             <div className="categories">
-            
+            <SliderHome/>
             </div>
             <div className="new-products"></div>
             <div className="shop-items">
+               <Couch/>
+                <Inter/>
                 <Chairs/>
-                <Chairs/>
-                <Chairs/>
-                <Chairs/>
+                <Bed/>
             </div>
-            
             <div className="delivery"></div>
             <div className="about-us"></div>
             <div className="contact-us">
               <ContactUs/>
             </div>
-            <div className="footer"></div>
+            <div className="footer">
+              <Footer/>
+            </div>
         </>
     )
   }
