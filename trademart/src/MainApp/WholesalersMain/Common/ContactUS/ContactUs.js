66f712466644edf78dd7ad5267b4c981368435f8
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-07 05:52:30 +0300

    Update

--- trademart/src/MainApp/WholesalersMain/Common/ContactUS/ContactUs.js
+++ trademart/src/MainApp/WholesalersMain/Common/ContactUS/ContactUs.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 function ContactUs() {
+  const Details = [
+    {
+      userStreet: "NGL-112, off View Street",
+      userEmail: "Sofastore@gmail.com",
+      userPhone: "+254-20311728"
+    }
+  ]
   return (
     <>
       <section className="contact">
@@ -10,30 +17,51 @@ function ContactUs() {
             elit. Repellat minima, consectetur excepturi odio assumenda laborum totam exercitationem
             atque sapiente. Aut, sint. Maxime at laudantium ex, quaerat dicta molestias? Non, perferendis?
           </p>
-        </div>
-        <div className="container contactInfo">
-          <div className="box">
-            <div className="icon"></div>
-            <div className="text">
-              <h3> Address</h3>
-              <p></p>
-            </div>
-          </div>
-          <div className="box">
-            <div className="icon"></div>
-            <div className="text">
-              <h3> Phone</h3>
-              <p></p>
-            </div>
-          </div>
-          <div className="box">
-            <div className="icon"></div>
-            <div className="text">
-              <h3> Email</h3>
-              <p></p>
-            </div>
-          </div>
-        </div>
+        </div>{
+          Details.map((value) => {
+            return (
+              <div className="container contactInfo">
+                <div className="box">
+                  <div className="icon"><i className="fa fa-map-marker"></i></div>
+                  <div className="text">
+                    <h3> Address</h3>
+                    <p>{value.userStreet}</p>
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="icon"><i className="fa fa-phone"></i></div>
+                  <div className="text">
+                    <h3> Phone</h3>
+                    <p>{value.userPhone}</p>
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="icon"><i className="fa fa-envelope"></i></div>
+                  <div className="text">
+                    <h3> Email</h3>
+                    <p>{value.userEmail}</p>
+                  </div>
+                </div>
+                <div className="contactForm">
+                  <form action="">
+                    <h2>Send Message</h2>
+                    <div className="inputBox">
+                      <input type="text" />
+                      <span></span>
+                    </div>
+                    <div className="inputBox">
+                      <input type="text" />
+                      <span></span>
+                    </div>
+                    <div className="inputBox">
+                      <input type="text" />
+                      <span></span>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            )
+          })}
       </section>
     </>
   )
--- trademart/src/MainApp/WholesalersMain/wholesaler.js
+++ trademart/src/MainApp/WholesalersMain/wholesaler.js
@@ -14,9 +14,9 @@ export class Wholesaler extends Component {
   render() {
     return (
       <>
-      <CommonHeader/>
+
       <FurnitureWholesaler/>
-      <ContactUs/>
+  
       </>
     )
   }
