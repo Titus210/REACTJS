9a59c587ea2719ba2aa6e4fb78ce756bc094be5b
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-11 22:57:43 +0300

    new

--- trademart/src/MainApp/AdminDashBoard/Components/SalesProfits/salesNav.js
+++ trademart/src/MainApp/AdminDashBoard/Components/SalesProfits/salesNav.js
@@ -1,32 +1,23 @@
 import React from 'react'
-import Charts from '../Overview/Charts/Charts'
+
+import { BarC, LineC } from '../Overview/Charts/Charts';
+
 
 
 import "./sales.css"
 
-const image = require("./face.jpg")
 function Sales() {
+    const date= new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();  
+    let amount = "12,000";
+
+
     return (
         <>
             <div className="main-container">
-                <div className="navbar-header">
-                    <div className="name">
-                        <h3>Sofa<span>shop</span></h3>
-
-                    </div>
-                    <div className="icons-display">
-                        <div className="icons">
-                            <i className="fa fa-message"></i>
-                            <i className="fa fa-bell"></i>
-                        </div>
-
-                        <div className="image">
-                            <p className='img-holder'><img src={image} alt="img" /></p>
-                            <p className="name">Titus Kiplagat</p>
-                        </div>
-                    </div>
-                </div>
-
+               
                 <div className="sales-wrapper">
                     <div className="sales-cards">
 
@@ -72,23 +63,64 @@ function Sales() {
                     <div className="chart-displ">
                         <div className="statistics">
                             <h4>Statistics</h4>
-                            <Charts />
+                            <BarC/>
                         </div>
                         <div className="sales-chart">
                             <h4>Growth Chart</h4>
-                            <Charts />
+                          <LineC/>
                         </div>
                     </div>
                     <div className="recent-orders">
                         <div className="order-header">
-                            <h3></h3>
-                            <div className="icons"></div>
+                            <h3>Recent Orders</h3>
+                            <div className="icons">
+                                <i className="fa fa-cog"></i>
+                                <i className="fa fa-search"></i>
+                            </div>
 
                         </div>
-                        <div className="customer-header"></div>
+                        <div className="customer-header">
+                            <div className="number">
+                                <h4>Number</h4>
+                                <p>1</p>
+                                <p>2</p>
+                            </div>
+                            <div className="customer-id">
+                                <h4>Customer Id</h4>
+                                <p>#3114</p>
+                                <p>#2232</p>
+                            </div>
+                            <div className="CustomerName">
+                                <h4>Customer Name</h4>
+                                <p>Jerry S</p>
+                                <p>J. Howley</p>
+                            </div>
+                            <div className="item-name">
+                                <h4>Item Name</h4>
+                                <p>4 by 6 Bed</p>
+                                <p>Wodden Table</p>
+                            </div>
+                            <div className="order-date">
+                                <h4>Order Date</h4>
+                                <p>{day + "/" + month + "/" + year}</p>
+                                <p>{day + "/" + month + "/" + year}</p>
+                            </div>
+                            <div className="status">
+                                <h4>Status</h4>
+                                <p>2</p>
+                                <p>3</p>
+                            </div>
+                            <div className="price">
+                                <h4>Price</h4>
+                                <p>${amount}</p>
+                                <p>${amount}</p>
+                            </div>
+                        </div>
                         <div className="order-details"></div>
                     </div>
+        
                 </div>
+
             </div>
         </>
     )
--- trademart/src/MainApp/Pages/Pages.js
+++ trademart/src/MainApp/Pages/Pages.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 // Compnent Imports
 import Category from './LandingCategory/Category'
 import AboutUs from './About/About'
@@ -8,17 +8,23 @@ import Furniture from './Furniture/Furniture'
 import Grocery from './Grocery/Grocery'
 import Interior from './Interior/Interior'
 
+
+import Header from "../Header/Header"
+
+
 import "./Pages.css"
 function Pages() {
   return (
     <>
-  
+    <Header/>
     <Category/>
     <FlashSale/>
     <Grocery/>
     <Interior/>
     <Furniture/>
     <AboutUs/>
+
+
     </>
   )
 }
