4cd1fa32795a7d0ed19ff26c4413c4f981f3ace1
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-11-20 23:13:26 +0300

    Js

--- trademart/src/MainApp/App/App.js
+++ trademart/src/MainApp/App/App.js
@@ -21,7 +21,7 @@ export class App extends Component {
             <>
 
                 <Router>
-                    {/* <DashBoard /> */}
+                    <DashBoard />
                     <Routes>
                   
                         <Route path="/" element={<Pages />} />
