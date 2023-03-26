ddc3b1105060e4f0b6ae65bfbc50e09484c23326
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-03-26 21:23:15 +0300

    Main Javascript file

---
+++ doc-chat/Frontend/doctalk/src/index.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+// styles
+import "./MainApp/App"
+
+
+// main application
+import App from "./MainApp/App";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
