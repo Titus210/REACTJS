f07f1e18e05ba04759b49260b73ee5604ce9df8c
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-09 12:03:30 +0300

    tailwind configuration files

--- Grocery/grocery/tailwind.config.js
+++ Grocery/grocery/tailwind.config.js
@@ -1,34 +1,37 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
     extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+
       colors: {
-        'primary-green': '##76B852',
+        'primary-green': '#76B852',
         'primary-blue': '#0070C9',
-        'orange-yellow': '##FFA500',
+        'orange-yellow': '#FFA500',
         'white': '#FFFFFF',
         'black': '#000000',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
+        'orange': '#FF7849',
+        'green': '#13CE66',
+        'yellow': '#FFC82C',
         'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-        'purple': '#7e5bef',
+        'gray': '#8492A6',
+        'gray-light': '#D3DCE6',
+        'purple': '#7E5BEF',
+      },
+
+      fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
       },
     },
-    fontFamily: {
-      sans: ['Source Sans Pro', 'sans-serif'],
-    },
   },
+
   plugins: [],
-}
+};
