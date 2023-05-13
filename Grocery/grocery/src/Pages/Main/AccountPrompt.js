dc5c31d8d3db4c1b19bf5cc90a4f6450c1c0cc24
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-13 13:19:32 +0300

    feat: Created login and signin buttons yet to implement navigation using react router - created hover states on the buttons and rectified their responsiveness

--- Grocery/grocery/src/Pages/Main/AccountPrompt.js
+++ Grocery/grocery/src/Pages/Main/AccountPrompt.js
@@ -4,18 +4,25 @@ function AccountPrompt() {
     return (
         <>
             <div className=" bg-slate-100">
-                <div className="h-screen flex items-center justify-center md:w-screen">
-                    <div className="clear-screen h-3/6"></div>
-                    <div className="clear-screen h-3/6">
-                        <h1 className="text-center text-orange text-3xl font-bold">
-                            Green<span className="text-primary-green">Mart</span>
-                        </h1>
-                        <div className="account-button">
-                            <p>Create Account</p>
+                <div className="w-screen h-screen flex items-center justify-center md:w-screen relative ">
+                    <div className="clear-screen h-3/4"></div>
+                    <div className="flex justify-between flex-col w-48 h-1/4">
+                        <div className="logo">
+                            <h1 className="text-center text-orange text-3xl font-bold">
+                                Green<span className="text-primary-green">Mart</span>
+                            </h1>
                         </div>
-                    </div>
+                        <div className="account-button">
+                            <p className='p-2 text-center font-bold text-orange-yellow border border-gray  outline-0 rounded-xl hover:text-white hover:bg-primary-green'>Login</p>
+                        </div>
+                        <div className="account-button">
+                            <p className='p-2 text-center text-white border bg-orange-yellow  outline-0 rounded-xl  hover:bg-black hover:text-orange-yellow'>Create Account</p>
+                        </div>
 
-                  
+                    </div>
+                    <div className="clear-screen absolute bottom-36">
+                        <p className='text-xl'>Continue as <span className="text-blue-600 underline">Guest</span></p>
+                    </div>
                 </div>
             </div>
         </>
