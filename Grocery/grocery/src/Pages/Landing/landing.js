909df2863376dc59e40427cb8799cac2776077bf
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-27 00:35:04 +0300

    feat: added landing page

---
+++ Grocery/grocery/src/Pages/Landing/landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Common/navbar'
+
+function Landing() {
+  return (
+    <>
+   <div className='container flex flex-row justify-between h-screen mx-auto px-4 md\:px-0'>
+        <Navbar />
+   </div>
+    </>
+  )
+}
+
+export default Landing
