44b880980f1e1ab6a3f6714bbb9114c85b473126
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-27 01:36:20 +0300

    feat: fullu responsive menu bar created the logic for conditional rendering -  commented the code for each section ________________ remaining section: upon login update account details and opn accoun icon - update user name upon login

--- Grocery/grocery/src/Components/Common/navbar.js
+++ Grocery/grocery/src/Components/Common/navbar.js
@@ -1,10 +1,25 @@
-import React from "react";
-
-// import icons from react icons
+import React, { useState } from "react";
 import { VscAccount, VscBell } from "react-icons/vsc";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Navbar() {
+  // State for menu open/close
+  const [menuOpen, setMenuOpen] = useState(false);
+  // State for shopping cart visibility
+  const [cartVisible, setCartVisible] = useState(true);
+
+  // Function to toggle the menu open/close
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    setCartVisible(true); // Show the cart when the menu is closed
+  };
+
+  // Function to toggle the shopping cart visibility
+  const toggleCart = () => {
+    setCartVisible(!cartVisible);
+  };
+
   return (
     <>
       <div className="flex flex-row justify-between items-center w-full h-16 relative">
@@ -15,43 +30,76 @@ function Navbar() {
             </h1>
           </div>
         </div>
-        <div className="absolute flex top-0  h-screen text-white bg-black w-80 lg:text-black lg:flex flex-col  items-center ml-8 lg:flex-row lg:relative lg:h-auto lg:bg-inherit">
-          <h1 className="text-2xl mt-4 lg:mt-0 lg:text-lg font-semibold text-gray-500">
+
+        {/* Menu bar */}
+        <div
+          className={`absolute flex top-0 h-screen text-white bg-black w-80 lg:text-black lg:flex lg:flex-row flex-col items-center ml-8 lg:relative lg:h-auto lg:bg-inherit ${
+            menuOpen ? "" : "hidden lg:flex"
+          }`}
+        >
+          {/* Menu items */}
+          <h1 className="text-2xl mt-4 font-semibold text-gray-500 lg:ml-8 lg:mt-0 lg:text-lg">
             Home
           </h1>
-          <h1 className="text-2xl mt-4 lg:mt-0 lg:text-lg font-semibold text-gray-500 lg:ml-8">
+          <h1 className="text-2xl mt-4 font-semibold text-gray-500 lg:ml-8 lg:mt-0 lg:text-lg">
             Products
           </h1>
-          <h1 className="text-2xl mt-4 lg:mt-0 lg:text-lg font-semibold text-gray-500 lg:ml-8">
+          <h1 className="text-2xl mt-4 font-semibold text-gray-500 lg:ml-8 lg:mt-0 lg:text-lg">
             Shop
           </h1>
-          <h1 className="text-2xl mt-4 lg:mt-0 lg:text-lg font-semibold text-gray-500 lg:ml-8">
+          <h1 className="text-2xl mt-4 font-semibold text-gray-500 lg:ml-8 lg:mt-0 lg:text-lg">
             Contact
           </h1>
-          <div className="flex flex-col items-center w-full relative p-2 lg:hidden">
-            <VscAccount className="w-6 h-6 mb-3"  />
-            <p className="p-2 text-center text-white border bg-orange-yellow  outline-0 rounded-xl w-9/12  hover:bg-black hover:text-orange-yellow md:w-11/12">
+          {/* Login */}
+          <div className="flex flex-col items-center w-full absolute bottom-4 p-2 mt-4 lg:hidden">
+            <VscAccount className="hidden w-6 h-6 mb-3" />
+            <p className="p-2 text-center text-white border bg-orange-yellow outline-0 rounded-xl w-9/12 hover:bg-black hover:text-orange-yellow md:w-11/12">
               Login
             </p>
           </div>
         </div>
+
+        {/* Navigation icons */}
         <div className="flex flex-row items-center">
           <div className="flex flex-row items-center">
-            <div className="flex flex-row items-center ml-8">
-              <div className="flex flex-row items-center relative p-2  ">
-                <h1 className="text-lg font-semibold text-gray-500 absolute -top-2 -right-1 text-purple">
-                  0
-                </h1>
-                <FaShoppingCart className="w-6 h-6 ml-2 text-primary-green" />
-              </div>
-              <div className="hidden lg:flex flex-row items-center ml-8 relative p-2  ">
+            <div className="flex flex-row items-center ml-8 relative">
+              {/* Shopping cart */}
+              {cartVisible && (
+                <div className="absolute right-12 flex flex-row items-center lg:relative lg:right-0 p-2">
+                  <h1 className="text-lg font-semibold text-gray-500 absolute -top-2 -right-1 text-purple">
+                    0
+                  </h1>
+                  <FaShoppingCart className="w-6 h-6 ml-2 text-primary-green" />
+                </div>
+              )}
+              {/* Notification bell */}
+              <div className="hidden lg:flex flex-row items-center ml-8 relative p-2">
                 <h1 className="text-lg font-semibold text-gray-500 absolute -top-2 right-1 text-red-500">
                   0
                 </h1>
                 <VscBell className="w-6 h-6 text-orange" />
               </div>
-              <div className="flex flex-row items-center ml-8 relative p-2  ">
+              {/* Account icon */}
+              <div className="hidden flex-row items-center ml-8 relative p-2 lg:flex">
                 <VscAccount className="w-6 h-6" />
+              </div>
+              {/* Menu icon */}
+              <div
+                className={`flex flex-row items-center ml-8 absolute right-0 p-2 lg:hidden ${
+                  menuOpen ? "hidden" : ""
+                }`}
+                onClick={toggleMenu}
+              >
+                <FaBars className="w-6 h-6" />
+              </div>
+              {/* Close icon */}
+              <div
+                className={`flex text-white flex-row items-center ml-8 absolute right-0 p-2 lg:hidden ${
+                  !menuOpen ? "hidden" : ""
+                }`}
+                onClick={toggleMenu}
+              >
+                <AiOutlineClose className="w-6 h-6" />
               </div>
             </div>
           </div>
