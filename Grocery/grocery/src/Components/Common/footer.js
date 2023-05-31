b039e3c08576d75b903053ed3db77901c85130f0
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-31 20:31:33 +0300

    footer: added sections on footer - yet to implement syles

--- Grocery/grocery/src/Components/Common/footer.js
+++ Grocery/grocery/src/Components/Common/footer.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer
-        <p>Footer</p>
-        <p>Green mart</p>
+    <div>
+      <div className="flex justify-evenly items-center w-full relative bg-slate-600">
+        <div className="social-login flex flex-col">
+          <h1 className="text-center text-orange text-2xl font-bold">
+            Green<span className="text-primary-green">Mart</span>
+          </h1>
+        </div>
+        <div className="faqs">
+          <h1>FAQ</h1>
+          <p>Where are we located</p>
+          <p>What other services do we provide</p>
+        </div>
+
+        <div className="service">
+          <p>Home`</p>
+          <p>About us</p>
+          <p></p>
+          <p></p>
+        </div>
+        <div className="contact-us">
+          <input type="text" />
+          <button>Calls us</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
