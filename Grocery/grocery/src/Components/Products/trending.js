9ce367eb5379974ca396de1bc086ecfece1f35da
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-06-05 20:05:11 +0300

    feat: implemented the same features as features component - did the same for mobile : yet to implement when button is clicked image we use filtering option to filter fruits

--- Grocery/grocery/src/Components/Products/trending.js
+++ Grocery/grocery/src/Components/Products/trending.js
@@ -17,42 +17,49 @@ function truncateDescription(description) {
 function Trending() {
   const [visibleItems, setVisibleItems] = useState(6);
   const totalItems = fruits.length;
-
+  
   const handleLoadMore = () => {
-    const increment = 4; // Number of items to load on each "Load More" click
-    setVisibleItems((prevVisibleItems) =>
-      Math.min(prevVisibleItems + increment, totalItems)
-    );
+    const increment = window.innerWidth >= 1024 ? 10 : 6; // Change increment based on window width
+    const newVisibleItems = visibleItems + increment;
+    setVisibleItems(Math.min(newVisibleItems, totalItems));
   };
+  
 
   const isLoadMoreVisible = visibleItems < totalItems;
 
   return (
-    <>    
+    <>
       <h1 className="text-center text-bold text-3xl">Trending Products</h1>
-      <div className="flex flex-wrap gap-1">
+      <div className="flex flex-wrap  bg-slate-300 gap-y-1 md:gap-x-1 lg:gap-x-6 ">
         {fruits.slice(0, visibleItems).map((fruit) => (
           <div
-            className="mx-auto fruit-card flex flex-col items-stretch bg-slate-300 shadow-md rounded-lg  m-2 w-5/6 md:w-2/6 lg:w-3/12"
+            className=" fruit-card flex flex-col   bg-slate-200 shadow-lg   rounded-lg  m-2 w-5/12 md:w-1/4 lg:w-2/12  ml-4 lg:mx-0 lg:ml-3"
             key={fruit.id}
           >
-            <div className="product-image h-40">
+            <div className="product-image h-36 flex items-center justify-center">
               <img
                 src={fruit.image}
                 alt={fruit.name}
-                className="w-full h-full object-cover rounded-md"
+                className=" h-full   object-cover rounded-md"
               />
             </div>
-            <div className="product-details flex flex-col justify-between">
-              <div className="flex justify-between items-center mb-2">
-                <p className="font-semibold text-lg">{fruit.name}</p>
-                <FaHeart className="w-6 h-6" />
+            <div className="product-details flex flex-col  items-center h-32">
+              <div className="flex justify-center items-center mb-2 ">
+                <p className="font-semibold text-lg text-primary-green">
+                  {fruit.name}
+                </p>
+                {/* <FaHeart className="w-6 h-6" /> */}
               </div>
-              <div className="flex justify-between">
-                <p>{truncateDescription(fruit.description)}</p>
-                <p>${fruit.price.toFixed(2)}</p>
+              <div className="flex justify-evenly  w-7/12 items-center">
+                {/* <p>{truncateDescription(fruit.description)}</p> */}
+                <p className="text-sm line-through text-red-400 ">
+                  ${fruit.price.toFixed(2)}
+                </p>
+                <p className="text-lg text-primary-green">
+                  ${fruit.price.toFixed(2)}
+                </p>
               </div>
-              <button className="bg-primary-blue text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-600">
+              <button className="bg-primary-blue text-white py-2 px-4 mt-4 rounded-md w-10/12 hover:bg-blue-600">
                 Add to Cart
               </button>
             </div>
