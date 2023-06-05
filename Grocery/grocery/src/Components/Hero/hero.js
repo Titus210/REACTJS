dd0a7de90192d9aec41383f9b4ae2c374e23a01a
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-06-05 19:57:19 +0300

    feat: image margin rectification

--- Grocery/grocery/src/Components/Hero/hero.js
+++ Grocery/grocery/src/Components/Hero/hero.js
@@ -5,7 +5,7 @@ import "../../Styles/hero.css";
 function Hero() {
   return (
     <>
-      <div className="md:h-max">
+      <div className="md:h-max w-full mx-auto">
         <div className="flex flex-col justify-between items-center h-2/3 lg:flex-row-reverse">
           <div className="hero-image w-full h-96 rounded-3xl  lg:w-7/12"> </div>
           <div className="flex items-center justify-center flex-col md:justify-evenly  h-2/4 md:w-9/12 lg:w-5/12">
