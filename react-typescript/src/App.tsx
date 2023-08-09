2c307889bce3f1d2902e7932230cd26fa44da1d3
Titus210 <tituskiplagat210@gmail.com>
2023-08-09 14:38:07 +0300

    Array working in typescript

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -2,16 +2,39 @@ import React from 'react';
 import './App.css';
 import { Greet } from './Components/Greet';
 import { Person } from './Components/person';
+import { PersonList } from './Components/personList';
 
 function App() {
   const personName = {
     first: "Titus",
     last: 'Kiplagat',
   }
+
+
+  const nameList = [
+    {
+      first: "Titus",
+      last: "Kiplagat",
+    },
+    {
+      first: "Emily",
+      last: "Johnson",
+    },
+    {
+      first: "Alex",
+      last: "Smith",
+    },
+    {
+      first: "Maria",
+      last: "Garcia",
+    },
+  ];
+
   return (
     <div className="App">
       <Greet name={personName.first} messageCount={22} isLoggedIn={true} />
-      <Person name = {personName} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
