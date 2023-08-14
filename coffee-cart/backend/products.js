f69b997374c7e790bc33ecb9f46ccbbb942e9214
Titus210 <tituskiplagat210@gmail.com>
2023-08-14 18:53:18 +0300

    List of products

---
+++ coffee-cart/backend/products.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: 1,
+        name: "Mocha",
+        description: "Coffee bean made mocha",
+        price: 4.99,
+        image: "", // Add image URL here
+    },
+    {
+        id: 2,
+        name: "Cappuccino",
+        description: "Creamy and frothy cappuccino",
+        price: 3.49,
+        image: "", // Add image URL here
+    },
+    {
+        id: 3,
+        name: "Espresso",
+        description: "Strong and concentrated espresso",
+        price: 2.99,
+    }, {
+        id: 4,
+        name: "Latte",
+        description: "Smooth and creamy latte",
+        price: 7.49,
+        image: "", // Add image URL here
+    },
+    {
+        id: 5,
+        name: "Black Coffee",
+        description: "Classic black coffee",
+        price: 12.49,
+        image: "", // Add image URL here
+    }
+];
+
+
+module.exports = products
