526198792c0ee058093667409f0f34b197c415f2
Titus210 <tituskiplagat210@gmail.com>
2023-08-30 17:09:45 +0300

    Authentication using firebase setup authentication on signup

---
+++ Grocery/grocery/src/context/AuthContext.js
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from './AuthContext';
+
+import auth from '../firebase'
+
+
+const AuthContext = React.createContext();
+
+export function useAuth(params) {
+    return useContext(AuthContext)
+}
+
+
+
+export function AuthProvider({ childen }) {
+
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+        })
+        return unsubscribe
+    }, [])
+
+
+    const value = {
+        currentUser,
+        signup
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
