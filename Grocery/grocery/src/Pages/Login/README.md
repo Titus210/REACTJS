9ce0ea29213e15db4646f3dafe43396de6d3f01f
Titus210 <tituskiplagat210@gmail.com>
2023-08-31 18:53:23 +0300

    feat: Implement LoginPage Component
    Create the LoginPage component to provide users with a seamless and secure login experience.
    
    ## Features
    
    - User Authentication: Allow users to log in using their email and password.
    - Social Media Login: Enable users to log in through Facebook, Google, and Apple.
    - Error Handling: Display error messages for failed login attempts.
    - Remember Me: Implement a "Remember Me" option to keep users logged in.
    - Forgot Password: Provide a link for users to reset their password.
    - Continue as Guest: Allow users to proceed without logging in.
    
    ## Fixes/Solved
    
    - Addressed issues related to error handling and component layout.
    - Improved user experience during login attempts.

---
+++ Grocery/grocery/src/Pages/Login/README.md
@@ -0,0 +1,52 @@
+# LoginPage Component
+
+The `LoginPage` component provides a user-friendly login interface that integrates with Firebase Authentication. Users can log in using their email and password, and have the option to log in via social media platforms like Facebook, Google, and Apple.
+
+## Features
+
+### User Authentication:
+ Enables users to log in using their email and password credentials.
+### Social Media Login:
+ Offers the convenience of logging in through Facebook, Google, and Apple accounts.
+### Error Handling:
+ Displays informative error messages to guide users in case of login failures.
+### Remember Me: 
+Provides an option for users to stay logged in by remembering their credentials.
+### Forgot Password: 
+Allows users to reset their password in case they forget it.
+### Continue as Guest:
+ Offers the choice to proceed as a guest user without logging in.
+
+
+### Firebase Authentication Integration
+
+The component integrates with Firebase Authentication to securely handle user registration. It uses the `login` function from the `useAuth` context to login to exsisting account. If any errors occur during the login process, appropriate error messages are displayed.
+
+### Error Handling
+
+The component includes error handling mechanisms to display meaningful error messages to users. For instance, if the user attempts to sign up with an email that is not yet registered, an error message will be shown, and the user will be prompted to try again or log in.
+
+### Loading and Button States
+
+To provide a smooth user experience, the sign-up button has different states. While the login process is in progress, the button will be disabled and show a loading spinner. This prevents users from submitting the form multiple times and ensures that the application's state is accurately represented to the user.
+
+### Social Login Options
+
+Users have the option to sign up using their existing social media accounts, such as Facebook, Google, or Apple. These options provide convenience and familiarity for users who may prefer to use their existing credentials.
+
+## Usage
+
+1. Install the necessary dependencies using `npm install` or `yarn install`.
+
+2. Import the `LoginPage` component into your project:
+
+```jsx
+import SignUp from './path/to/Login';
+```
+
+3. Use the SignUp component within your application, placing it where you want the sign-up form to appear:
+
+```jsx
+<LoginPage>
+```
+
--- Grocery/grocery/src/Pages/Login/login.js
+++ Grocery/grocery/src/Pages/Login/login.js
@@ -42,7 +42,7 @@ function LoginPage() {
       // Display error message if login fails
       setError("Failed to login, continue as guest");
     }
-    
+
     setLoading(false);
     setClicked(false);
   }
@@ -51,19 +51,19 @@ function LoginPage() {
     <>
       {/* Main container */}
       <div className="w-screen md:w-11/12">
-        <div className="mx-auto h-screen w-screen flex flex-col justify items-center bg-[#214462] lg:flex-row lg:justify-evenly relative">
+        <div className="mx-auto h-screen w-screen flex flex-col justify items-center bg-gray-dark lg:flex-row lg:justify-evenly relative">
           {/* Background image */}
           <div className="hidden login-bg bg-cover bg-no-repeat bg-center justify-center items-center bg-slate-500 h-1/2 w-2/5 lg:flex"></div>
-          
+
           {/* Logo */}
           <div className="flex justify-center items-center w-full h-1/4 top-1 lg:absolute">
             <p className="text-3xl text-center text-orange-yellow md:text-5xl font-bold">
               Green<span className="text-primary-green">Mart</span>
             </p>
           </div>
-          
+
           {/* Login form */}
-          <div className="w-full h-3/4 flex flex-col justify-between items-center bg-slate-200 rounded-t-3xl shadow-2xl md:rounded-b-3xl md:w-2/4 lg:w-1/4 md:h-max md:border-0 md:p-2">
+          <div className="w-full h-3/4 flex flex-col justify-between items-center bg-[#2734447b] rounded-t-3xl p-2 shadow-2xl md:rounded-b-3xl md:w-2/4 lg:w-1/4 md:h-max md:border-0 ">
             <form action="" className="w-full py-3 relative" onSubmit={handleSubmit}>
               {/* Welcome message */}
               <div className="text-start">
@@ -71,81 +71,81 @@ function LoginPage() {
                   Welcome Back {currentUser && currentUser.email}
                 </p>
               </div>
-              
+
               {/* Error message */}
               {error &&
                 <div className="error flex items-center justify-center py-2">
                   <p className="w-11/12 bg-red-400 h-full text-center p-2 text-white font-bold text-xl">{error}</p>
                 </div>
               }
-              
+
               {/* Input fields */}
               <div className="flex flex-col justify-start items-center w-full">
-                <label htmlFor="email" className="ml-4 self-start text-primary-blue text-xl">
+                <label htmlFor="email" className="ml-4 self-start text-white text-xl mb-2">
                   Enter your email
                 </label>
                 <input type="text" ref={emailRef} className="border-gray w-11/12 p-2 rounded-lg bg-slate-300" />
-                
-                <label htmlFor="password" className="ml-4 mt-4 self-start text-primary-blue text-xl">
+
+                <label htmlFor="password" className="ml-4 mt-4 self-start text-white mb-2 text-xl">
                   Enter your password
                 </label>
                 <input type="text" ref={passwordRef} className="border-gray w-11/12 p-2 rounded-lg bg-slate-300" />
               </div>
-              
+
               {/* Additional actions */}
               <div className="flex flex-row justify-evenly w-full mt-3">
                 <div className="flex flex-row">
                   <input type="checkbox" name="remember me" id="remember-me" />
-                  <p className="text-xl ml-1 lg:text-lg">Remember me</p>
+                  <p className="text-xl text-white ml-1 lg:text-lg">Remember me</p>
                 </div>
                 <p className="text-primary-blue text-xl underline lg:text-lg">
-                  <Link to="/forgot-password" className="text-primary-blue underline">forgot password</Link>
+                  <Link to="/forgot-password" className="text-green underline">forgot password</Link>
                 </p>
               </div>
-              
+
               {/* Login button */}
               <div className="login-button w-full flex justify-center mt-5">
                 <p className={`p-2 text-center text-white border bg-orange-yellow outline-0 rounded-xl w-9/12 hover:bg-black hover:text-orange-yellow md:w-11/12 ${clicked ? 'clicked' : ''}`} onClick={handleSubmit} type='button' disabled={loading}>
                   Login
                 </p>
               </div>
-              
+
               {/* Social login options */}
               <div className="flex flex-col justify-center items-center w-full mt-5">
                 <div className="flex flex-row items-center justify-center mb-4 w-9/12 lg:mb-2 md:w-11/12">
                   <div className="line"></div>
-                  <p className="text-xl">OR</p>
+                  <p className="text-xl text-white">OR</p>
                   <div className="line"></div>
                 </div>
                 <div className="flex flex-row justify-evenly items-center w-7/12">
                   <FaFacebook className="w-6 h-6 t text-blue-500 cursor-pointer hover:scale-125" />
                   <FaGoogle className="w-6 h-6 t text-red-500 cursor-pointer hover:scale-125" />
-                  <FaApple className="w-6 h-6 t text-gray-500 cursor-pointer hover:scale-125" />
+                  <FaApple className="w-6 h-6 t text-black cursor-pointer hover:scale-125" />
                 </div>
                 <div className="flex flex-row items-center justify-center mt-3 w-9/12 md:w-11/12">
-                  <p className=" ">dont have an account {" "} <span>
-                    <Link to="/signup" className="text-primary-blue underline">sign up</Link>
+                  <p className="text-white ">dont have an account {" "} <span>
+                    <Link to="/signup" className="text-green underline">sign up</Link>
                   </span></p>
                 </div>
               </div>
-              
+
               {/* Continue as guest */}
               <div className="hidden w-full justify-center mb-4 md:flex">
-                <p className="text-xl">
+                <p className="text-xl text-white">
                   Continue as{" "}
                   <span className="text-primary-blue underline">
-                    <Link to="/landing" className="text-primary-blue underline">guest</Link>
+                    <Link to="/landing" className="text-green underline">guest</Link>
                   </span>
                 </p>
               </div>
             </form>
-            
+
             {/* Continue as guest (mobile) */}
             <div className="absolute bottom-12 flex w-full justify-center mb-4 md:hidden">
-              <p className="text-xl">
+              <p className="text-xl text-white">
                 Continue as{" "}
-                <span className="text-primary-blue underline">
-                  <Link to="/landing" className="text-primary-blue underline">guest</Link>
+                <span className="text-green underline">
+                  <Link to="/landing" className="text-green underline">guest</Link>
                 </span>
               </p>
             </div>
