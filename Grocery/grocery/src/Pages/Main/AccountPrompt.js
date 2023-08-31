05dfd2ff6d4a58db4e610da8205879574ba13c5f
Titus210 <tituskiplagat210@gmail.com>
2023-08-31 11:45:11 +0300

    Account prompt setup    -features:        - Login button        - create button

--- Grocery/grocery/src/Pages/Main/AccountPrompt.js
+++ Grocery/grocery/src/Pages/Main/AccountPrompt.js
@@ -1,28 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import './mainPage.css'
 
 function AccountPrompt() {
     return (
         <>
-            <div className=" bg-slate-100">
-                <div className="w-screen h-screen flex items-center justify-center md:w-screen relative ">
+            <div className="account-prompt bg-styles">
+                <div className="w-screen h-screen flex items-center justify-center md:w-screen relative blur-background">
                     <div className="clear-screen h-3/4"></div>
                     <div className="flex justify-between flex-col w-48 h-1/4">
                         <div className="logo">
-                            <h1 className="text-center text-orange text-3xl font-bold">
+                            <h1 className="text-center text-orange  text-6xl font-bold">
                                 Green<span className="text-primary-green">Mart</span>
                             </h1>
                         </div>
                         <div className="account-button">
-                            <p className='p-2 text-center font-bold text-orange-yellow border border-gray  outline-0 rounded-xl hover:text-white hover:bg-primary-green'>Login</p>
+                            <p className='p-2 text-center font-bold text-white border border-gray  outline-0 rounded-xl hover:text-white bg-black hover:bg-primary-green'>
+                                <Link to='/login'> Login
+                                </Link></p>
                         </div>
                         <div className="account-button">
-                            <p className='p-2 text-center text-white border bg-orange-yellow  outline-0 rounded-xl  hover:bg-black hover:text-orange-yellow'>Create Account</p>
+                            <p className='p-2 text-center text-white border bg-black  outline-0 rounded-xl  hover:bg-green'>
+                                <Link to='/signup'> Create Account
+                                </Link>
+                            </p>
                         </div>
 
                     </div>
-                    <div className="clear-screen absolute bottom-36">
-                        <p className='text-xl'>Continue as <span className="text-blue-600 underline">Guest</span></p>
-                    </div>
+
                 </div>
             </div>
         </>
--- Grocery/grocery/src/Pages/Main/MainPage.js
+++ Grocery/grocery/src/Pages/Main/MainPage.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './mainPage.css';
 
 function MainPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay of 5 seconds
+    const delay = 5000; // 5 seconds in milliseconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      // Navigate to the /account-prompt page
+      window.location.href = '/account-prompt';
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return null; // Don't render anything while loading
+  }
+
   return (
-    <>
-      <div className=" bg-slate-100">
+    <div className="main-page">
       <div className="h-screen flex items-center justify-center md:w-screen">
-          <h1 className="text-center text-orange text-3xl font-bold">
+        <div className="blur-background">
+          <h1 className="text-center text-orange text-9xl font-bold">
             Green<span className="text-primary-green">Mart</span>
-        </h1>
+          </h1>
+        </div>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
 export default MainPage;
