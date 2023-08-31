601d83e044807fdc3d5ca5dc4411eb0bbf078426
Titus210 <tituskiplagat210@gmail.com>
2023-08-31 18:55:05 +0300

    feat: File setup - file dependencies configuration

--- Grocery/grocery/tailwind.config.js
+++ Grocery/grocery/tailwind.config.js
@@ -14,16 +14,19 @@ module.exports = {
 
       colors: {
         'primary-green': '#76B852',
+        'secondary-green': '#002A22',
         'primary-blue': '#0070C9',
-        'orange-yellow': '#FFA500',
-        'white': '#FFFFFF',
+        'orange-yellow': '#FF7849',
+        'white': '#F7F5F7',
         'black': '#000000',
         'orange': '#FF7849',
         'green': '#13CE66',
         'yellow': '#FFC82C',
-        'gray-dark': '#273444',
+        'gray-dark': '#214462',
+        'grayish-blue': " #273444",
+        'secondary-gray': ' #214461',
         'gray': '#8492A6',
-        'gray-light': '#D3DCE6',
+        'gray-light': '#2B394A',
         'purple': '#7E5BEF',
       },
 
