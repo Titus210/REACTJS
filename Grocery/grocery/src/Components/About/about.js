be7e0edad21815338a80f0e2a434847aa65d51aa
Titus210 <tituskiplagat210@gmail.com>
2023-08-31 18:57:38 +0300

    feat: Create About Component
    Implement the About component to provide information about the company's offerings and values.
    
    ## Features
    
    - Display About Us section with a title and description.
    - Show a farmer image to represent the company's farm-fresh produce.
    - Provide a "Read More" button for users to learn more about the company.
    
    ## Usage
    
    1. Import the `About` component from the specified path.
    2. Include the `About` component within your application's page.
    
    ## Styles
    
    Ensure the `about.css` stylesheet is properly imported for consistent styling.

--- Grocery/grocery/src/Components/About/about.js
+++ Grocery/grocery/src/Components/About/about.js
@@ -1,23 +1,34 @@
 import React from "react";
-
 import "../../Styles/about.css";
+
 function About() {
   return (
     <>
-      <h1 className="text-center text-bold text-3xl">About Us</h1>
+      {/* About Us Section */}
+      <h1 className="text-center text-orange font-bold text-bold text-3xl">
+        About Us
+      </h1>
       <div className="flex flex-col lg:flex-row items-center justify-evenly w-11/12 mx-auto">
-        <div className="farmer-image  w-full md:w-3/4 h-96 md:h-80 rounded-3xl lg:w-7/12  hover:bg-slate-400 shadow-sm shadow-opacity-50 hover:shadow-md hover:shadow-opacity-100"></div>
+        {/* Farmer Image */}
+        <div className="farmer-image w-full md:w-2/4 h-96 md:h-80 rounded-3xl lg:w-4/12 shadow-sm shadow-opacity-50 hover:shadow-md hover:shadow-opacity-100"></div>
+        
+        {/* Description and Read More */}
         <div className="flex-flex-col w-full lg:w-2/4">
-          <div className="flex items-center justify-center">
-            <p className="text-center text-lg  text-white md:text-xl">
-              We offer variety of fruits, giving the best fruits right from the
-              local farms. We offer variety of fruits, giving the best fruits
-              right from the local farms. We offer variety of fruits, giving the
-              best fruits right from the local farms.. <span className="text-primary-blue underline">read more</span>
+          <div className="flex flex-col items-center justify-center">
+            {/* Description Text */}
+            <p className="text-center text-lg text-white md:text-xl">
+              We offer a variety of fruits, providing the best fruits sourced
+              directly from local farms. Our commitment is to deliver high-quality
+              and fresh produce to our customers.
+            </p>
+            {/* Read More Button */}
+            <p className="p-2 text-center border-0 text-xl font-bold bg-orange-yellow outline-0 rounded-lg mt-4 hover:bg-black hover:text-orange-yellow w-5/12 md:w-4/12">
+              Read More
             </p>
           </div>
+          {/* Display Icons (if any) */}
           <div className="display-icons">
-       
+            {/* Add any icons or content related to the about section */}
           </div>
         </div>
       </div>
