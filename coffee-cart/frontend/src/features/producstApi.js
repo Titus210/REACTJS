a8ad8a8c24e3cef63896be80daa9e5ed00acbab2
Titus210 <tituskiplagat210@gmail.com>
2023-09-20 23:57:38 +0300

    feat: Added react routing - productsApi fetches data from database - fetch products

--- coffee-cart/frontend/src/features/producstApi.js
+++ coffee-cart/frontend/src/features/producstApi.js
@@ -3,5 +3,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
     reducerPath: 'productsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' })
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+    endpoints: (builder) => ({
+        getAllProducts: builder.query({
+            guery: () => "products"
+        })
+    })
 })
+
+export const { useGetAllProductsQuery } = productsApi;
