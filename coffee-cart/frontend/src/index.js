e24c6a0e5ead9f5145d7dd275954f1c7be147bdf
Titus210 <tituskiplagat210@gmail.com>
2023-10-16 23:52:25 +0300

    feat: Redux  Cofee REdx Configuration

--- coffee-cart/frontend/src/index.js
+++ coffee-cart/frontend/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import productReducer, { productsFetch } from './features/productSlice';
+import { productsApi } from './features/producstApi';
+
+const store = configureStore({
+  reducer: {
+    products: productReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware(),
+    productsApi.middleware,
+  ],
+});
+
+store.dispatch(productsFetch());
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root') 
 );
-
