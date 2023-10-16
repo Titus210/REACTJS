dab0a482f04a9b0d80fe89c2e6dbe709b56e39f3
Titus210 <tituskiplagat210@gmail.com>
2023-10-16 23:51:50 +0300

    fea: Advance Filtering - Setting up Sidebar Search Component - Configuring action Calls

---
+++ advance-filtering/src/sidebar/colors/Colors.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+
+import "../Sidebar.css";
+import Input from '../../components/Input';
+
+const Colors = ({ handleChange }) => {
+  return (
+    <>
+      <div>
+        <h2 className="sidebar-title color-title">Colors</h2>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test3" />
+          <span className="checkmark all"></span>
+          All
+        </label>
+
+        <Input
+          handleChange={handleChange}
+          value="black"
+          title="Black"
+          name="test3"
+          color="black"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="blue"
+          title="Blue"
+          name="test3"
+          color="blue"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="red"
+          title="Red"
+          name="test3"
+          color="red"
+        />
+
+        <Input
+          handleChange={handleChange}
+          value="green"
+          title="Green"
+          name="test3"
+          color="green"
+        />
+
+        <label className="sidebar-label-container">
+          <input
+            onChange={handleChange}
+            type="radio"
+            value="white"
+            name="test3"
+          />
+          <span
+            className="checkmark"
+            style={{ background: "white", border: "2px solid black" }}
+          ></span>
+          White
+        </label>
+      </div>
+    </>
+  );
+};
+
+export default Colors;
