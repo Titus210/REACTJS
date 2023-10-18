eb46bd134cf13ed5e5b0d37cfa975a9fa9c1f1ce
Titus210 <tituskiplagat210@gmail.com>
2023-10-18 22:42:56 +0300

    feat:  Homepage This is homepage category cards

---
+++ Grocery/grocery/src/Pages/homepage/Homepage.js
@@ -0,0 +1,56 @@
+import Footer from "../../Components/Common/footer/footer";
+import Navbar from "../../Components/Common/navbar/NavBar";
+import Newsletter from "../../Components/Newsletter/newletter";
+import ShopCategories from "../../Components/ui/category/ShopCategories";
+import Hero from "../../Components/ui/hero/Hero";
+import ProductList from "../../Components/ui/products/ProductList";
+import AboutSection from "../about/About";
+
+const Homepage = () => {
+    return (
+        <main>
+            {/* Navbar */}
+            <nav className="">
+                <Navbar />
+            </nav>
+
+            {/* Hero */}
+            <section className="hero">
+                <Hero bgSize="550px"/>
+            </section>
+            {/* Category */}
+            
+            <section className="category">
+                <ShopCategories />
+            </section>
+            {/* Quick Cards */}
+
+            {/* Products */}
+            <section className="products">
+                <ProductList />
+            </section>
+
+            {/* About */}
+            <section className="about">
+                <AboutSection />
+            </section>
+
+            {/*Why choose us */}
+            <section className="category">
+                <ShopCategories />
+            </section>
+
+            {/* Newsletter */}
+            <section className="news-letter">
+                <Newsletter />
+            </section>
+
+            {/* Footer */}
+            <section className="footer">
+                <Footer />
+            </section>
+        </main>
+    );
+}
+
+export default Homepage;
