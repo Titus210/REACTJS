a940f837be397f2f0fc4b21a531966cfa8646655
Titus210 <tituskiplagat210@gmail.com>
2024-01-12 07:25:58 +0300

    fix: Added div to contain the button
    - Changed  Color to updated colors in tw.css

--- Grocery/grocery/src/Components/Common/button/PrimaryButton.js
+++ Grocery/grocery/src/Components/Common/button/PrimaryButton.js
@@ -1,7 +1,7 @@
 const PrimaryButton = ({btnText}) => {
     return (
         <div className="button">
-        <button className="bg-primary-green text-white py-2 px-4 rounded-md  text-xl font-bold hover:bg-accent-orange">
+        <button className="bg-primary-green text-white py-2 px-4 rounded-md  text-xl  hover:bg-accent-yellow">
             {btnText}
         </button>
         </div>
