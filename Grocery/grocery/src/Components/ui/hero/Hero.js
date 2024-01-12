00c4732150e2393d8af617f5d03a46d10e91b660
Titus210 <tituskiplagat210@gmail.com>
2024-01-12 07:30:29 +0300

    fix: Structure change
    descr:
    Changed Strcuture and text information from the paragraph test
    
    Included heroStyles(internal css)

--- Grocery/grocery/src/Components/ui/hero/Hero.js
+++ Grocery/grocery/src/Components/ui/hero/Hero.js
@@ -1,18 +1,27 @@
 import SecondaryButton from '../../Common/button/SecondaryButton';
 
+import backgroundImage from '../../../Assets/Images/background-1.jpg'
+import PrimaryButton from '../../Common/button/PrimaryButton';
 
-const Hero = ({ bgSize}) => {
+
+const heroStyles ={
+    backgroundImage : `url(${backgroundImage})`,
+    backgroundSize:'cover',
+    backgroundPosition:'center'
+}
+
+const Hero = () => {
     return (
         <>
-            <section className={`hero h-[550px] md:h-[bg-${bgSize}]`} >
-                <div className="wrapper h-full bg-primary-green flex flex-col justify-center items-center ">
+            <section className={`hero h-[500px] md:h-[750px]`}  >
+                <div className="wrapper h-full flex flex-col justify-center items-center gap-6">
                     <div className="text-center">
-                        <p className="text-xl">Welcome to our Farm</p>
-                        <h1 className="text-6xl font-bold">VEGETABLES</h1>
-                        <p className="text-xl">Welcome to our Farm</p>
+                        <p className="text-xl ">Welcome to our fresh</p>
+                        <h1 className="text-6xl font-bold text--green uppercase text-primary-green">Grocery</h1>
+                        <p className="text-xl">get organic food for your shop</p>
                     </div>
                     <div className="button">
-                        <SecondaryButton btnText="Welcome" />
+                        <PrimaryButton btnText="Welcome" />
                     </div>
                 </div>
             </section >
