1c8782b4a9302711bc4885272528195797d6868a
Titus210 <tituskiplagat210@gmail.com>
2024-01-12 03:06:30 +0300

    MOD: Using grocery Appropriate colors - Categorized colors to primary, accent etc.
    fix: N/A

--- Grocery/grocery/tailwind.config.js
+++ Grocery/grocery/tailwind.config.js
@@ -1,4 +1,3 @@
-// tailwind.config.js
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -13,25 +12,32 @@ module.exports = {
       },
 
       colors: {
-        'primary-green': '#76B852',
-        'secondary-green': '#002A22',
-        'primary-blue': '#0070C9',
-        'orange-yellow': '#FF7849',
-        'white': '#F7F5F7',
-        'black': '#000000',
-        'orange': '#FF7849',
-        'green': '#13CE66',
-        'yellow': '#FFC82C',
-        'gray-dark': '#214462',
-        'grayish-blue': " #273444",
-        'secondary-gray': ' #214461',
-        'gray': '#8492A6',
-        'gray-light': '#2B394A',
-        'purple': '#7E5BEF',
+        'primary': {
+          'green': '#76B852',
+          'blue': '#0070C9',
+        },
+        'secondary': {
+          'green': '#002A22',
+          'gray': '#214461',
+        },
+        'accent': {
+          'orange': '#FF7849',
+          'yellow': '#FFC82C',
+        },
+        'neutral': {
+          'white': '#F7F5F7',
+          'black': '#000000',
+          'gray-dark': '#214462',
+          'grayish-blue': '#273444',
+          'gray-light': '#2B394A',
+        },
+        'special': {
+          'purple': '#7E5BEF',
+        },
       },
 
       fontFamily: {
-        sans: ['Source Sans Pro', 'sans-serif'],
+        'sans': ['Open Sans', 'sans-serif'],
       },
     },
   },
